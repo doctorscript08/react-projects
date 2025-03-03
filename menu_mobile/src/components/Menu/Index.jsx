58a3b3ef5a2f087480f151247100a4c1db5d59dd
doctorscript08 <kelsondesousa99@gmail.com>
2025-03-03 16:24:26 +0100

    menu-mobile

---
+++ menu_mobile/src/components/Menu/Index.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import "./menu.css"
+
+function Menu() {
+  const [isOpen, setisOpen] = useState(false)
+
+  return (
+    <>
+      <header className="cabecalho">
+        <div>
+          <h1>Logo</h1>
+          <button className="open btn" onClick={() => setisOpen(!isOpen)}><i className="fa-solid fa-bars-staggered"></i></button>
+        </div>
+      </header>
+      <nav className={`nav-bar ${isOpen ? "open" : ""}`}>
+        <button className="close btn" onClick={() => setisOpen(!isOpen)}><i className="fa-solid fa-xmark"></i></button>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre nós</a></li>
+          <li><a href="#">Ajuda</a></li>
+          <li><a href="#">Terminar Sessão</a></li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Menu
